f466b1a51ac796e210184d2f9a0d7bcfb1094bb7
Caio Arlan Gomes <108774432+caioarlan18@users.noreply.github.com>
2022-10-30 17:46:36 -0300

    first commit

---
+++ loja51.js
@@ -0,0 +1,8 @@
+function MenuClick() {
+    var icon = document.querySelector('#icon')
+    var itens = document.querySelector('#itens')
+    itens.classList.toggle('active')
+
+}
+
+alert('Esse é um site demonstrativo feito por Caio Arlan com HTML, CSS & JAVASCRIPT')
